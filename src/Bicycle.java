2f48dacbb50b09443a405c153c41be5df573af35
MalikNegmatov <maliknegmatov@gmail.com>
2023-06-13 15:28:42 +0300

    Добавили интерфейс, с помощью которого реализовали по возрастающей возможности сервиса.

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -3,6 +3,7 @@
             super(modelName, wheelsCount);
         }
         public void updateTyre() {
+// Добавили ещё строку "Обслуживаем "модель". Родительский класс, достаточно один раз.
             System.out.println("Обслуживаем " + this.getModelName());
             for (int i = 0; i < this.getWheelsCount(); i++) {
                 System.out.println("Меняем покрышку");
